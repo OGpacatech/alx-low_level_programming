1708412b72822e8ca15dc3b4b4a3bec09265b817
OGpacatech <140067270+OGpacatech@users.noreply.github.com>
2023-09-13 05:46:50 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,34 +3,32 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-
 void times_table(void)
 {
-	int x, y, z;
+	int i, j, k;
 
-	for (x = 0; x < 10; x++)
+	for (i = 0; i < 10; i++)
 	{
-		for (y = 0; y < 10; y++)
+		for (j = 0; j < 10; j++)
 		{
-			z = x * y;
-
-      			if (y == 0)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar(z + '0');
+				_putchar(k + '0');
 			}
-			if (z < 10 && y != 0)
+
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(z + '0');
-			} 
-         		else if (z >= 10)
+				_putchar(k + '0');
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((z / 10) + '0');
-				_putchar((z % 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
