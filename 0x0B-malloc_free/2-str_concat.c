8c2254936d91c6f1aa8966dc4de7307a4c73c39b
OGpacatech <og.pacatech@.com>
2023-10-04 20:57:26 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,43 +2,38 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenate two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: concatenated string, or NULL on failure
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concat string
  */
+
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int i, j, len1 = 0, len2 = 0;
+	char *a;
+	int i, j, c, d;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-    while (s2[len2] != '\0')
-        len2++;
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
+		return (NULL);
 
-    concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	for (c = 0, d = 0; c < (i + j + 1); c++)
+	{
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
+	}
 
-    if (concat == NULL)
-        return (NULL);
-
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
-
-    for (j = 0; j < len2; j++)
-    {
-        concat[i] = s2[j];
-        i++;
-    }
-
-    concat[i] = '\0';
-
-    return (concat);
+	return (a);
 }
